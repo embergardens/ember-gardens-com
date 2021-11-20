89cb962ac13019d1a46f7c9fb172238c09872fc1
Tim Spears <timspears@imarc.com>
2021-11-20 14:46:05 -0500

    add icons

---
+++ src/components/icons/IconArrowSimple.js
@@ -0,0 +1,9 @@
+/* eslint-disable arrow-body-style */
+import React from 'react'
+
+export const IconArrowSimple = () => {
+
+   return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.02 11.28"><path d="M18.79,5.19,13,.15a.65.65,0,0,0-.91.08.64.64,0,0,0,.07.89L16.59,5H.64A.64.64,0,0,0,0,5.63a.64.64,0,0,0,.64.64H16.76L14.21,8.55l-2,1.57a.64.64,0,0,0,.75,1l0,0,2-1.6,3.78-3.37a.63.63,0,0,0,0-.9Z" fill="#715191"/></svg>
+   )
+}
---
+++ src/components/icons/IconLogoMountains.js
@@ -0,0 +1,9 @@
+/* eslint-disable arrow-body-style */
+import React from 'react'
+
+export const IconLogoMountains = () => {
+
+   return (
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 473.78 237.49"><defs><linearGradient id="a" x1="-340.88" y1="633.2" x2="-340.88" y2="633.68" gradientTransform="matrix(535.71, 0, 0, -494.42, 182847.4, 313302.47)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#715091"/><stop offset="0.5" stop-color="#ba5b80"/><stop offset="1" stop-color="#ff8672"/></linearGradient><linearGradient id="b" x1="-340.58" y1="633.28" x2="-340.58" y2="633.76" gradientTransform="matrix(308.43, 0, 0, -412.84, 105120.65, 261680.45)" xlink:href="#a"/><linearGradient id="c" x1="-341.73" y1="633.28" x2="-341.73" y2="633.76" gradientTransform="matrix(308.43, 0, 0, -412.84, 105798.53, 261681.11)" xlink:href="#a"/></defs><g opacity="0.85"><path d="M349.11,209H155.88L185.13,155h136L237,0,108.23,237.49H365.55ZM237.47,58.51l36.18,66.71H201.28Z" fill="url(#a)"/><path d="M45.92,208.9l62-115.25L133.55,141l14.6-26.91L107.54,39.19,0,237.49H81.21l15.5-28.59H45.92" fill="url(#b)"/><path d="M427.86,208.9l-62-115.25L340.22,141l-14.59-26.91,40.6-74.87,107.55,198.3H392.56L377.07,208.9h50.79" fill="url(#c)"/></g></svg>
+   )
+}
