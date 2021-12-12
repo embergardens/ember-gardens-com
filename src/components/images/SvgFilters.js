88eac15593e2abfe55092f7d35bfb20cb08129e5
Tim Spears <timspears@imarc.com>
2021-12-11 21:49:29 -0500

    safari fixes

---
+++ src/components/images/SvgFilters.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export const SvgFilters = () => {
+   const svgStyle = {
+      height: 0,
+      left: 0,
+      position: 'fixed',
+      top: 0,
+      zIndex: -2000
+   }
+
+   return(
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 0 0" style={svgStyle}>
+         <filter id="ember-haze" x="-10%" y="-10%" width="120%" height="120%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+            <feColorMatrix
+               type="matrix"
+               values="1 0 0 0 0
+                     1 0 0 0 0
+                     1 0 0 0 0
+                     0 0 0 1 0"
+               in="SourceGraphic"
+               result="colormatrix"
+            />
+            <feComponentTransfer in="colormatrix" result="componentTransfer">
+               <feFuncR type="table" tableValues="0.44 0.73 0.96"/>
+               <feFuncG type="table" tableValues="0.32 0.35 0.52"/>
+               <feFuncB type="table" tableValues="0.57 0.5 0.45"/>
+               <feFuncA type="table" tableValues="0 0.8"/>
+            </feComponentTransfer>
+            <feBlend mode="multiply" in="componentTransfer" in2="SourceGraphic" result="blend"/>
+         </filter>
+      </svg>
+   )
+}
--- src/components/layout/ContentWrapper.js
+++ src/components/layout/ContentWrapper.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import BackgroundMedia from '../images/BackgroundMedia'
 import BackgroundGradient from '../images/BackgroundGradient'
+import { SvgFilters } from '../images/SvgFilters'
 
 export const ContentWrapper = ( { children, layout, image, gradient } ) => {
    const layoutClass = layout ? `-${ layout }` : '-full'
 
    return (
       <main className={ `contentWrapper ${ layoutClass }` }>
-         { gradient &&
+         <SvgFilters />
+         {/* { gradient &&
             <BackgroundGradient theme={ gradient } />
-         }
+         } */}
          { image &&
             <BackgroundMedia image={ image } />
          }
