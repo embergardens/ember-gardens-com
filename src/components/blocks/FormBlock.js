6ec19ceb62942026554bd91d4231834645c9dbe0
Tim Spears <timspears@imarc.com>
2022-02-12 23:06:34 -0500

    update tabbing

--- src/components/blocks/FormBlock.js
+++ src/components/blocks/FormBlock.js
@@ -6,32 +6,30 @@ import { useStaticQuery, graphql } from 'gatsby'
 import GravityFormForm from 'gatsby-plugin-gravity-forms'
 
 export const FormBlock = ({ formId }) => {
-  const data = useStaticQuery(graphql`
-    {
-      allWpGfForm {
-        nodes {
-          ...gfForm
-        }
+   const data = useStaticQuery(graphql`
+      {
+         allWpGfForm {
+            nodes {
+               ...gfForm
+            }
+         }
       }
-    }
-  `)
+   `)
 
-  const {
-    allWpGfForm: { nodes },
-  } = data
+   const {
+      allWpGfForm: { nodes },
+   } = data
 
-  const form = nodes.filter((node) => {
-    if (node.databaseId === Number(formId.gravityFormSelect)) {
-      return node
-    }
-  })
+   const form = nodes.filter((node) => {
+      if (node.databaseId === Number(formId.gravityFormSelect)) {
+         return node
+      }
+   })
 
-  console.log({ form })
-
-  return (
-    <div className="formBlock">
-      <h1>GRAVITY FORM</h1>
-      {/* <GravityFormForm data={ data } /> */}
-    </div>
-  )
+   return (
+      <div className="formBlock">
+         <h1>GRAVITY FORM</h1>
+         <GravityFormForm data={ form[0] } />
+      </div>
+   )
 }
