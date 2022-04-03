5975ef4361423bbf6b7e8f355a4c43ed5a3a56d1
Tim Spears <timspears@imarc.com>
2022-04-03 15:34:54 -0400

    update 404 page & Favicon

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,54 @@
 import React from 'react'
-import { ContentWrapper } from '../components/layout/ContentWrapper';
+import { graphql, useStaticQuery } from "gatsby"
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
+import { ContentWrapper } from '../components/layout/ContentWrapper'
+import { IconLogoWhite } from '../components/icons/IconLogoWhite'
+import Seo from '../components/layout/Seo'
 
 const NotFound = ({location}) => {
+   // DATA ====================================================
+   const data = useStaticQuery( graphql`
+      query NotFoundData {
+         options: wp {
+            ...Gateway
+         }
+      }
+   `)
+
+   const { options: { acfOptionsGlobal: { globalOptions: { gatewayOptions } } } } = data
+   const { backgroundImage: image } = gatewayOptions
+   const imageData = getImage(image?.localFile)
 
    return (
       <>
-         <div className='notFound-template'>
-            <ContentWrapper>
-               <h1>808s &amp; 404s</h1>
-               <p>Sorry, { location.href } is not a page.</p>
-            </ContentWrapper>
+         <Seo title='404: Not Found' />
+         <div className='notFound-template gateway -notFound'>
+            <div className='gateway__overlay -notFound'>
+               { imageData &&
+                  <GatsbyImage
+                     alt={image.altText}
+                     image={imageData}
+                     className='gateway__backgroundImage'
+                     placeholder="blurred"
+                  />
+               }
+               <div className='gateway__content'>
+                  <div className='gateway__contentWrapper'>
+                     <div className='gateway__body'>
+                        <h4 className='gateway__title'>
+                           Feeling lost?
+                        </h4>
+                        <div className='gateway__subtitle'>
+                           It looks like <br /><strong>{ location.href }</strong><br /> is not a page or can't be found.
+                        </div>
+                     </div>
+                     <div className='gateway__footer'>
+                        <IconLogoWhite />
+                     </div>
+                  </div>
+               </div>
+            </div>
          </div>
       </>
 
