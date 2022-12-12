84dd2a11c2f47346307d3d8dcbc0a5128d352cea
Tim Spears <tspears1@gmail.com>
2022-12-12 09:36:29 -0500

    buttonBlock style fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -21,13 +21,6 @@ import "./src/css-dev/style.scss"
 
 // eslint-disable-next-line arrow-body-style
 export const wrapPageElement = ({ element, props }) => {
-   // if ( props?.pageResources?.page?.path === '/404.html' ) {
-   //    return (
-   //       <RecoilRoot>
-   //          {element}
-   //       </RecoilRoot>
-   //    )
-   // }
 
    return (
       <RecoilRoot>
